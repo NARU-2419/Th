a067a3fe7197639a4cbc14828ab1ec3e08793a5a
NARU-2419 <chauhannaru6@gmail.com>
2025-02-11 17:45:21 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -13,11 +13,11 @@ from threading import Thread
 
 loop = asyncio.get_event_loop()
 
-TOKEN = '7323'
+TOKEN = '7854678216:AAHjEPFY-Ji_GRg_VZZIkUL_z-aBk02y3zw'
 MONGO_URI = 'mongodb+srv://rishi:ipxkingyt@rishiv.ncljp.mongodb.net/?retryWrites=true&w=majority&appName=rishiv'
-FORWARD_CHANNEL_ID = -1002234
-CHANNEL_ID = -10022342
-error_channel_id = -10022
+FORWARD_CHANNEL_ID = -1002190354568
+CHANNEL_ID = -1002190354568
+error_channel_id = -1002190354568
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
